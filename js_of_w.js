19838d3b993b62b1dcb1b918bf5f96442e51d5e8
catless137 <catlesscatnt@gmail.com>
2026-06-10 18:07:28 -0600

    v0

---
+++ js_of_w.js
@@ -0,0 +1,7 @@
+function mouse_fun() {
+  document.button.style.backgroundColor = Math.random() > 0.5 ? "gold" : "silver";
+}
+
+function opinion_submit() {
+  alert("wasaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+}
